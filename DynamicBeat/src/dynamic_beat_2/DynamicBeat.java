5248fb330716705a66205ce0b25c81b2b683637d
Han Jang <Han Jang@DESKTOP-QH7RG8L.fios-router.home>
2019-04-20 23:11:38 -0400

    Double Buffering Comments

--- DynamicBeat/src/dynamic_beat_2/DynamicBeat.java
+++ DynamicBeat/src/dynamic_beat_2/DynamicBeat.java
@@ -28,15 +28,16 @@ public class DynamicBeat extends JFrame{
 	}
 	
 	public void paint(Graphics g) {
-		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-		screenGraphic = screenImage.getGraphics();
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT); //an empty off-screen image is created
+		screenGraphic = screenImage.getGraphics(); //off-screen image's graphic is saved to the variable
 		screenDraw(screenGraphic);
-		g.drawImage(screenImage, 0, 0, null);
+		g.drawImage(screenImage, 0, 0, null);//The image which is drawn in the off-screen is passed to this screen
 				
 	}
 	
+	//The method draws the introBackground to the off-screen image to 
 	public void screenDraw(Graphics g) {
 		g.drawImage(introBackground, 0, 0, null);
-		this.repaint();
+		this.repaint(); //paint() cannot be directly called
 	}
 }
